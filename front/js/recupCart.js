29a4e1f961d550c65ff489bed6a8107fb78462c9
Tonio <contact@wispelaere.fr>
2022-10-07 12:26:20 +0200

    fix logic off total function and adjust selector to remove item

--- front/js/recupCart.js
+++ front/js/recupCart.js
@@ -1,4 +1,4 @@
-import cartItem from './tmpl/cartItem.js'
+import cartItem from "./tmpl/cartItem.js";
 
 //au clique sur panier récupéré les données stockées dans localstorage
 //vérifier le nombre déléments de local storage pour une boucle for et crée les différentes lignes de produits
@@ -8,177 +8,212 @@ const storageArticles = JSON.parse(localStorage.getItem("products"));
 let articles = [];
 let recupQuantity = [];
 let recupTotalPrice = [];
-let buttonSubmitValid = document.getElementById('order');
-buttonSubmitValid.setAttribute('disabled', true);
-buttonSubmitValid.style.opacity = '0.3';
-console.log('storageArticles', storageArticles.length);
+let buttonSubmitValid = document.getElementById("order");
+buttonSubmitValid.setAttribute("disabled", true);
+buttonSubmitValid.style.opacity = "0.3";
+console.log("storageArticles", storageArticles.length);
 if (storageArticles === null || storageArticles.length === 0) {
-    const err = 'E016';
+    const err = "E016";
     error(err);
 } else {
-
     storageArticles.forEach((article, idx) => {
         const urlProducts = "http://localhost:3000/api/products/" + article.id;
         fetchData(urlProducts)
-            .then(
-                function recuperationById(dataResult) {
-                    let price = dataResult["price"];
-                    let name = dataResult["name"];
-                    let alt = dataResult["altTxt"];
-                    let imageUrl = dataResult["imageUrl"];
-                    let articleCartItem = document.createElement('article');
-                    let totalProductPrice = price * article.quantity;
-                    articleCartItem.classList.add("cart__item");
-                    articleCartItem.setAttribute('data-id', `${article.id}_${article.color}`);
-                    articleCartItem.setAttribute('data-color', article.color);
-                    articleCartItem.innerHTML = cartItem;
-                    articleCartItem.querySelector('.cart__item__img__content').src = imageUrl;
-                    articleCartItem.querySelector('.cart__item__img__content').setAttribute('alt', alt);
-                    articleCartItem.querySelector('.cart__item__title').innerText = name;
-                    articleCartItem.querySelector('.cart__item__color').innerText = article.color;
-                    articleCartItem.querySelector('.cart__item__price').innerText = price;
-                    articleCartItem.querySelector('.itemQuantity').value = article.quantity;
-                    document.querySelector('#cart__items').appendChild(articleCartItem);
-                    articleCartItem.querySelector('.itemQuantity').setAttribute('data-id', `${article.id}_${article.color}`);
-                    articles.push(dataResult);
-                    recupQuantity.push(article.quantity);
-                    recupTotalPrice.push(totalProductPrice);
+            .then(function recuperationById(dataResult) {
+                let price = dataResult["price"];
+                let name = dataResult["name"];
+                let alt = dataResult["altTxt"];
+                let imageUrl = dataResult["imageUrl"];
+                let articleCartItem = document.createElement("article");
+                let totalProductPrice = price * article.quantity;
+                articleCartItem.classList.add("cart__item");
+                articleCartItem.setAttribute(
+                    "data-id",
+                    `${article.id}_${article.color}`
+                );
+                articleCartItem.setAttribute("data-color", article.color);
+                articleCartItem.innerHTML = cartItem;
+                articleCartItem.querySelector(".cart__item__img__content").src =
+                    imageUrl;
+                articleCartItem
+                    .querySelector(".cart__item__img__content")
+                    .setAttribute("alt", alt);
+                articleCartItem.querySelector(".cart__item__title").innerText =
+                    name;
+                articleCartItem.querySelector(".cart__item__color").innerText =
+                    article.color;
+                articleCartItem.querySelector(".cart__item__price").innerText =
+                    price;
+                articleCartItem.querySelector(".itemQuantity").value =
+                    article.quantity;
+                document
+                    .querySelector("#cart__items")
+                    .appendChild(articleCartItem);
+                articleCartItem
+                    .querySelector(".itemQuantity")
+                    .setAttribute("data-id", `${article.id}_${article.color}`);
+                articles.push(dataResult);
+                recupQuantity.push(article.quantity);
+                recupTotalPrice.push(totalProductPrice);
 
-                    //changement de la quantité
-                    let inputQuantity = articleCartItem.querySelector('.itemQuantity');
-                    inputQuantity.addEventListener('change', function (e) {
-                        let newQuantity = e.target.value;
-                        let cartProducts = JSON.parse(localStorage.getItem('products'))
+                //changement de la quantité
+                let inputQuantity =
+                    articleCartItem.querySelector(".itemQuantity");
+                inputQuantity.addEventListener("change", function (e) {
+                    let newQuantity = e.target.value;
+                    let cartProducts = JSON.parse(
+                        localStorage.getItem("products")
+                    );
+                    let idProductsModifie = article.id;
+                    let color = article.color;
+                    let newProducts = {
+                        id: idProductsModifie,
+                        color: color,
+                        quantity: parseInt(newQuantity)
+                    };
+
+                    for (let i = 0; cartProducts.length > i; i++) {
+                        let quantityModified = cartProducts[i];
+                        if (idProductsModifie === quantityModified.id) {
+                            if (color === quantityModified.color) {
+                                cartProducts.splice(i, 1, newProducts);
+                                // localStorage.removeItem('products')
+                                localStorage.setItem(
+                                    "products",
+                                    JSON.stringify(cartProducts)
+                                );
+                            }
+                            total();
+                        }
+                    }
+                });
+                //buttonDelete permet à l'utilisateur de supprimer la ligne concernée
+                let buttonDelete = articleCartItem.querySelector(".deleteItem");
+                if (buttonDelete) {
+                    buttonDelete.addEventListener("click", function (e) {
+                        let cartProducts = JSON.parse(
+                            localStorage.getItem("products")
+                        );
                         let idProductsModifie = article.id;
                         let color = article.color;
-                        let newProducts = {
-                            id: idProductsModifie,
-                            color: color,
-                            quantity: parseInt(newQuantity)
-                        };
 
                         for (let i = 0; cartProducts.length > i; i++) {
-                            let quantityModified = cartProducts[i];
-                            if (idProductsModifie === quantityModified.id) {
+                            let verifIdColor = cartProducts[i];
+                            if (idProductsModifie === verifIdColor.id) {
+                                if (color === verifIdColor.color) {
+                                    let cuurentElementDelete =
+                                        document.querySelectorAll(
+                                            `[data-id="${article.id}_${article.color}"]`
+                                        );
 
-                                if (color === quantityModified.color) {
-                                    cartProducts.splice(i, 1, newProducts);
-                                    // localStorage.removeItem('products')
-                                    localStorage.setItem('products', JSON.stringify(cartProducts));
-
-                                }
-                                total();
-                            }
-                        }
-
-
-                    })
-                    //buttonDelete permet à l'utilisateur de supprimer la ligne concernée
-                    let buttonDelete = articleCartItem.querySelector('.deleteItem')
-                    if (buttonDelete) {
-                        buttonDelete.addEventListener('click', function (e) {
-                            let cartProducts = JSON.parse(localStorage.getItem('products'))
-                            let idProductsModifie = article.id;
-                            let color = article.color;
-                            for (let i = 0; cartProducts.length > i; i++) {
-                                let verifIdColor = cartProducts[i];
-                                if (idProductsModifie === verifIdColor.id) {
-
-                                    if (color === verifIdColor.color) {
-                                        let emplacementElementDelete = document.getElementsByClassName('cart_item');
-                                        emplacementElementDelete.setAttribute('hidden', true);
-                                        cartProducts.splice(i, 1);
-                                        localStorage.removeItem('products')
-                                        localStorage.setItem('products', JSON.stringify(cartProducts));
-                                        //supprimer la ligne de l'article sans reload
+                                    cuurentElementDelete[0].setAttribute(
+                                        "hidden",
+                                        true
+                                    );
+                                    cuurentElementDelete[0].classList.add(
+                                        "cart__item__hidden"
+                                    );
+                                    cartProducts.splice(i, 1);
+                                    localStorage.removeItem("products");
+                                    localStorage.setItem(
+                                        "products",
+                                        JSON.stringify(cartProducts)
+                                    );
+                                    console.log(
+                                        "after suppress cartProducts",
+                                        cartProducts
+                                    );
+                                    if (cartProducts.length == 0) {
+                                        console.log('plus de panier, remoove form price etc etc');
                                     }
-                                    total();
+                                    //supprimer la ligne de l'article sans reload
                                 } else {
-                                    // pas le bon id 
+                                    // pas le bon id
                                 }
                             }
-                        });
-                    }
-                    //récuperer la taille du panier lacalsotrage puis l'index de l'occurence 
-                    if (storageArticles.length === idx + 1) {
-                        total();
-                    }
+                            setTimeout(() => {
+                                total();
+                            }, 500);
+                        }
+                    });
                 }
-
-            )
+                //récuperer la taille du panier lacalsotrage puis l'index de l'occurence
+                if (storageArticles.length === idx + 1) {
+                    total();
+                }
+            })
             .catch(function (err) {
                 // Une erreur est survenue
             });
-
     });
     //on vérifie ici les regex sur l'événement change pour le nom
 
-    const firstNameInput = document.getElementById('firstName');
-    const lastNameInput = document.getElementById('lastName');
-    const cityInput = document.getElementById('city');
-    buttonSubmitValid.style.opacity = '0.3';
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+    const cityInput = document.getElementById("city");
+    buttonSubmitValid.style.opacity = "0.3";
     let globalError = {
         firstName: null,
         lastName: null,
         city: null
     };
-    firstNameInput.addEventListener('change', function (e) {
+    firstNameInput.addEventListener("change", function (e) {
         const value = e.target.value;
         verifInputChange(value);
 
         if (verifInputChange(value) === false) {
-            let err = 'E008';
+            let err = "E008";
             error(err);
             globalError.firstName = null;
-            console.log('globalError', globalError);
+            console.log("globalError", globalError);
         } else {
-            let emptyError = '';
-            let emplacementNoError = document.getElementById("firstNameErrorMsg");
+            let emptyError = "";
+            let emplacementNoError =
+                document.getElementById("firstNameErrorMsg");
             emplacementNoError.innerHTML = emptyError;
             globalError.firstName = true;
-            console.log('globalError', globalError);
+            console.log("globalError", globalError);
         }
         accesValidation(globalError);
         if (accesValidation(globalError) === true) {
-            buttonSubmitValid.setAttribute('disabled', false);
-            buttonSubmitValid.style.opacity = '1';
-            console.log('test');
+            buttonSubmitValid.setAttribute("disabled", false);
+            buttonSubmitValid.style.opacity = "1";
+            console.log("test");
         }
     });
     //vérification au changement pour le prénom
-    lastNameInput.addEventListener('change', function (e) {
+    lastNameInput.addEventListener("change", function (e) {
         const value = e.target.value;
         verifInputChange(value);
 
         if (verifInputChange(value) === false) {
-            let err = 'E009';
+            let err = "E009";
             error(err);
             globalError.lastName = null;
-
         } else {
-            let emptyError = '';
-            let emplacementNoError = document.getElementById("lastNameErrorMsg");
+            let emptyError = "";
+            let emplacementNoError =
+                document.getElementById("lastNameErrorMsg");
             emplacementNoError.innerHTML = emptyError;
             globalError.lastName = true;
         }
         accesValidation(globalError);
         if (accesValidation(globalError) === true) {
-            buttonSubmitValid.setAttribute('disabled', false);
-            buttonSubmitValid.style.opacity = '1';
+            buttonSubmitValid.setAttribute("disabled", false);
+            buttonSubmitValid.style.opacity = "1";
         }
     });
     //vérification au changement pour la ville
-    cityInput.addEventListener('change', function (e) {
+    cityInput.addEventListener("change", function (e) {
         const value = e.target.value;
         verifInputChange(value);
 
-        if (verifInputChange(value) === false || value === '') {
-            let err = 'E010';
+        if (verifInputChange(value) === false || value === "") {
+            let err = "E010";
             error(err);
             globalError.city = null;
-
         } else {
-            let emptyError = '';
+            let emptyError = "";
             let emplacementNoError = document.getElementById("cityErrorMsg");
             emplacementNoError.innerHTML = emptyError;
             globalError.city = true;
@@ -186,25 +221,25 @@ if (storageArticles === null || storageArticles.length === 0) {
         accesValidation(globalError);
         if (accesValidation(globalError) === true) {
             buttonSubmitValid.removeAttribute("disabled");
-            buttonSubmitValid.style.opacity = '1';
+            buttonSubmitValid.style.opacity = "1";
         }
     });
     //on gére la validation de la commande en vérifiant les informations entre puis en intégrant au local storage un objet contact
 
-    const buttonSubmit = document.getElementById('order');
-    buttonSubmit.addEventListener('click', function (e) {
+    const buttonSubmit = document.getElementById("order");
+    buttonSubmit.addEventListener("click", function (e) {
         let idProducts = [];
         let firstName = document.getElementById("firstName").value;
-        let lastName = document.getElementById('lastName').value;
-        let address = document.getElementById('address').value;
-        let city = document.getElementById('city').value;
-        let mail = document.getElementById('email').value;
-        const localProducts = JSON.parse(localStorage.getItem('products'));
+        let lastName = document.getElementById("lastName").value;
+        let address = document.getElementById("address").value;
+        let city = document.getElementById("city").value;
+        let mail = document.getElementById("email").value;
+        const localProducts = JSON.parse(localStorage.getItem("products"));
         let idProductForOrder = [];
 
         if (globalError.firstName && globalError.lastName && globalError.city) {
             // Push Product ID in idProductForOrder
-            localProducts.forEach(product => {
+            localProducts.forEach((product) => {
                 idProductForOrder.push(product.id);
             });
             let bodyData = {
@@ -216,17 +251,15 @@ if (storageArticles === null || storageArticles.length === 0) {
                     email: mail
                 },
                 products: idProductForOrder
-
             };
             const optionsPost = {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
                 },
                 body: JSON.stringify(bodyData)
-            }
-
+            };
 
             //requete POST pour envoyer l'objet contact
             fetch("http://localhost:3000/api/products/order", optionsPost)
@@ -236,18 +269,14 @@ if (storageArticles === null || storageArticles.length === 0) {
                 .then((data) => {
                     // function pageConfirmation(data) {
                     let orderId = data.orderId;
-                    const idPageConfirmation = "http://127.0.0.1:5500/front/html/confirmation.html?id=" + orderId;;
+                    const idPageConfirmation =
+                        "http://127.0.0.1:5500/front/html/confirmation.html?id=" +
+                        orderId;
                     window.location.href = idPageConfirmation;
-
                 })
                 .catch(function (err) {
                     //erreur
                 });
-
         }
     });
-
 }
-
-
-
